7b2a8b04518095fcedb291ce30de9bf214d43844
Vertti Ahlstén <vertti.ahlsten@hiq.fi>
2024-01-29 13:33:19 +0200

    Tests updated

--- Frends.Instagram.Request/Frends.Instagram.Request.Tests/UnitTests.cs
+++ Frends.Instagram.Request/Frends.Instagram.Request.Tests/UnitTests.cs
@@ -32,50 +32,6 @@ public class UnitTests
     }
 
     [Test]
-    public void TestGetInsights()
-    {
-        var input = new Input
-        {
-            Method = Methods.GET,
-            QueryParameters = "metric=page_impressions_unique&metric=post_reactions_love_total",
-            Reference = "insights",
-            AccessToken = token,
-            ApiVersion = "18.0",
-        };
-
-        var ret = Instagram.Request(input, new Options(), default);
-        Assert.IsNotNull(ret);
-
-        /* Test user has no permissions for this and request currently returns an error code.
-         * Once permissions are added, assert should be replaced with:
-         * Assert.AreEqual(ret.Result.Statuscode, 200);
-         */
-        Assert.AreNotEqual(ret.Result.Statuscode, 200);
-    }
-
-    [Test]
-    public void TestGetADS()
-    {
-        var input = new Input
-        {
-            Method = Methods.GET,
-            Reference = "ads_archive",
-            QueryParameters = "ad_reached_countries=ALL&ad_type=POLITICAL_AND_ISSUE_ADS",
-            AccessToken = token,
-            ApiVersion = "18.0",
-        };
-
-        var ret = Instagram.Request(input, new Options(), default);
-        Assert.IsNotNull(ret);
-
-        /* Test user has no permissions for this and request currently returns an error code.
-         * Once permissions are added, assert should be replaced with:
-         * Assert.AreEqual(ret.Result.Statuscode, 200);
-         */
-        Assert.AreNotEqual(ret.Result.Statuscode, 200);
-    }
-
-    [Test]
     public void TestGetOther()
     {
         var input = new Input
@@ -88,24 +44,6 @@ public class UnitTests
         };
 
         var ret = Instagram.Request(input, new Options { ThrowErrorOnFailure = true }, default);
-        Assert.IsNotNull(ret);
-        Assert.AreEqual(ret.Result.Statuscode, 200);
-        Assert.IsTrue(ret.Result.Message.Contains(objectId));
-    }
-
-    [Test]
-    public void TestGetOtherWithDuplicateParameters()
-    {
-        var input = new Input
-        {
-            Method = Methods.GET,
-            Reference = "me",
-            QueryParameters = "fields=id&fields=name",
-            AccessToken = token,
-            ApiVersion = "18.0",
-        };
-
-        var ret = Instagram.Request(input, new Options(), default);
         Assert.IsNotNull(ret);
         Assert.AreEqual(ret.Result.Statuscode, 200);
         Assert.IsTrue(ret.Result.Message.Contains(objectId));
@@ -127,24 +65,7 @@ public class UnitTests
     }
 
     [Test]
-    public async Task TestPostToPageAsync()
-    {
-        var input = new Input
-        {
-            Method = Methods.POST,
-            Reference = objectId + "/feed",
-            AccessToken = token,
-            ApiVersion = "18.0",
-            Message = "{ \"message\": \"This is a test.\" }",
-        };
-
-        var ret = await Instagram.Request(input, new Options(), default);
-        Assert.IsNotNull(ret);
-        Assert.AreNotEqual(ret.Statuscode, 200);
-    }
-
-    [Test]
-    public void TestThroErrorOnFailure()
+    public void TestThrowErrorOnFailure()
     {
         var input = new Input
         {
@@ -207,6 +128,81 @@ public class UnitTests
         var ret = Assert.ThrowsAsync<ArgumentNullException>(() => Instagram.Request(input, new Options(), default));
         Assert.IsNotNull(ret);
     }
+
+    // --
+
+    [Test]
+    public async Task Get_Me_Success()
+    {
+        var input = new Input
+        {
+            Reference = "me",
+            QueryParameters = "fields=id,name,accounts,business_users",
+            ApiVersion = "18.0",
+            AccessToken = token,
+        };
+
+        var options = new Options() { ThrowErrorOnFailure = true };
+
+        var result = await Instagram.Request(input, options, default);
+        Assert.AreEqual(200, result.Statuscode);
+        Assert.IsNotNull(result.Message);
+    }
+
+    [Test]
+    public async Task Get_Reference_Success()
+    {
+        var input = new Input
+        {
+            Reference = "6727087737314510",
+            QueryParameters = null,
+            ApiVersion = "18.0",
+            AccessToken = token,
+        };
+
+        var options = new Options() { ThrowErrorOnFailure = true };
+
+        var result = await Instagram.Request(input, options, default);
+        Assert.AreEqual(200, result.Statuscode);
+        Assert.IsNotNull(result.Message);
+    }
+
+    [Test]
+    public async Task Get_ThrowErrorOnFailure_False()
+    {
+        var input = new Input
+        {
+            Reference = "123",
+            QueryParameters = "me?fields=id,name,accounts,business_users",
+            ApiVersion = "18.0",
+            AccessToken = token,
+        };
+
+        var options = new Options() { ThrowErrorOnFailure = false };
+
+        var result = await Instagram.Request(input, options, default);
+        Assert.AreEqual(400, result.Statuscode);
+        Assert.IsNotNull(result.Message);
+    }
+
+    [Test]
+    public async Task Get_ThrowErrorOnFailure_True()
+    {
+        var input = new Input
+        {
+            Reference = "123",
+            QueryParameters = "me?fields=id,name,accounts,business_users",
+            ApiVersion = "18.0",
+            AccessToken = token,
+        };
+
+        var options = new Options() { ThrowErrorOnFailure = true };
+
+        var result = Assert.ThrowsAsync<Exception>(() => Instagram.Request(input, options, default));
+        Assert.IsNotNull(result.Message);
+    }
+
+    // --
 
     private static async Task<JObject> GetAsync(string url, string token)
     {
